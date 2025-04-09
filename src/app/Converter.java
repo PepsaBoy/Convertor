5efb6be786bca50fc375ea866c5ee7dd7f7f1752
PepsaBoy <Serebro.75@ukr.net>
2025-04-09 21:41:07 +0300

    Add miles to km

--- src/app/Converter.java
+++ src/app/Converter.java
@@ -1,8 +1,15 @@
 package app;
 
 public class Converter {
+    private static final double CONV_K = 1.609;
     public static void main(String[] args) {
         System.out.println("Distance conversion app");
+        double mil=10;
+        double km = convMiltoKm(mil);
+        System.out.println("Result is " +km);
+    }
+    public static double convMiltoKm(double mil){
+        return mil*CONV_K;
     }
 
 }
