3af444268231321ef61194a14aa8cf6d1779e176
PepsaBoy <Serebro.75@ukr.net>
2025-04-09 21:45:07 +0300

    Add km to miles

--- src/app/Converter.java
+++ src/app/Converter.java
@@ -5,11 +5,16 @@ public class Converter {
     public static void main(String[] args) {
         System.out.println("Distance conversion app");
         double mil=10;
-        double km = convMiltoKm(mil);
-        System.out.println("Result is " +km);
+        double km=15;
+        double killometres = convMiltoKm(mil);
+        double miles = convKmtoMil(km);
+        System.out.println("Miles to km is " +killometres+ " / Km to miles " +miles);
     }
     public static double convMiltoKm(double mil){
         return mil*CONV_K;
+    }
+    public static double convKmtoMil(double km){
+        return km/CONV_K;
     }
 
 }
